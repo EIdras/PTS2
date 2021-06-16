d39776dec6afb66eecd44e9138b4ad383db3d9b2
Eldras <simon72610@gmail.com>
2021-06-16 19:46:21 +0200

    Gestion d'erreur pendant l'enregistrement
    - Message de confirmation de l'enregistrement d'un fichier
    - Message d'erreur/avertissement si des champs non renseignés sont requis pour l'enregistrement
    - Activation / Désactivation des boutons suivant le mode choisi et choix du mode de base
    - Bouton "Fermer" qui permet de fermer l'exercice en cours en revenant au menu

--- PTS2/src/application/FileManager.java
+++ PTS2/src/application/FileManager.java
@@ -93,7 +93,7 @@ public class FileManager {
 
 	public void sauvegarderFichier(String exoName, String path, String consigne, String script, List<String> aide,
 			String pathToMedia, String occultationChar, String mode, int authorizeIncompleteBooleanWord,
-			int numberOfLetterThatCanBeDiscover, int displayFoundWords, int displayFinalAnswer, int TimeLimitInMinute) {
+			int numberOfLetterThatCanBeDiscover, int displayFoundWords, int displayFinalAnswer, int TimeLimitInMinute) throws IOException {
 		// Initialisation variable
 		Scanner scanner = new Scanner(System.in);
 		final int BUFFERSIZE = 4 * 1024;
@@ -103,9 +103,9 @@ public class FileManager {
 		String outputFilePath = path + "\\" + exoName;
 		int tailleVideo = 0;
 
-		try ( // Ecriture de la vidéo dans un fichier tampon
+		// Ecriture de la vidéo dans un fichier tampon
 				FileInputStream fin = new FileInputStream(new File(sourceFilePath));
-				FileOutputStream fout = new FileOutputStream(new File("fichierTampon.bin"));) {
+				FileOutputStream fout = new FileOutputStream(new File("fichierTampon.bin"));
 
 			byte[] buffer = new byte[BUFFERSIZE];
 
@@ -114,63 +114,58 @@ public class FileManager {
 				++tailleVideo;
 				fout.write(buffer, 0, bytesRead);
 			}
-		} catch (Exception e) {
-			System.out.println("Something went wrong! Reason: " + e.getMessage());
 
-		}
 
-		try ( // Ecriture du fichier exo
-				FileInputStream fin = new FileInputStream(new File("fichierTampon.bin"));
-				FileOutputStream fout = new FileOutputStream(new File(outputFilePath + ".exo"));) {
+		// Ecriture du fichier exo
+				FileInputStream finF = new FileInputStream(new File("fichierTampon.bin"));
+				FileOutputStream foutF = new FileOutputStream(new File(outputFilePath + ".exo"));
 
-			byte[] buffer = new byte[BUFFERSIZE];
+			byte[] bufferF = new byte[BUFFERSIZE];
 
-			fout.write(String.valueOf(tailleVideo).getBytes()); // Ecriture taille video
-			fout.write(0x00);
-			fout.write(String.valueOf(exoName.length()).getBytes()); // Ecriture taille texte
-			fout.write(0x00);
-			fout.write(String.valueOf(consigne.length()).getBytes()); // Ecriture taille consigne
-			fout.write(0x00);
-			fout.write(String.valueOf(script.length()).getBytes()); // Ecriture taille script
-			fout.write(0x00);
-			fout.write(String.valueOf(aide.toString().length()).getBytes()); // Ecriture taille aide
-			fout.write(0x00);
-			fout.write(String.valueOf(occultationChar.length()).getBytes()); // Ecriture taille char d'occultation
-			fout.write(0x00);
-			fout.write(String.valueOf(mode.length()).getBytes()); // Ecriture taille mode
-			fout.write(0x00);
-			fout.write(String.valueOf(authorizeIncompleteBooleanWord).getBytes()); // Ecriture taille autoriser mot
+			foutF.write(String.valueOf(tailleVideo).getBytes()); // Ecriture taille video
+			foutF.write(0x00);
+			foutF.write(String.valueOf(exoName.length()).getBytes()); // Ecriture taille texte
+			foutF.write(0x00);
+			foutF.write(String.valueOf(consigne.length()).getBytes()); // Ecriture taille consigne
+			foutF.write(0x00);
+			foutF.write(String.valueOf(script.length()).getBytes()); // Ecriture taille script
+			foutF.write(0x00);
+			foutF.write(String.valueOf(aide.toString().length()).getBytes()); // Ecriture taille aide
+			foutF.write(0x00);
+			foutF.write(String.valueOf(occultationChar.length()).getBytes()); // Ecriture taille char d'occultation
+			foutF.write(0x00);
+			foutF.write(String.valueOf(mode.length()).getBytes()); // Ecriture taille mode
+			foutF.write(0x00);
+			foutF.write(String.valueOf(authorizeIncompleteBooleanWord).getBytes()); // Ecriture taille autoriser mot
 																					// incomplet
-			fout.write(0x00);
-			fout.write(String.valueOf(numberOfLetterThatCanBeDiscover).getBytes()); // Ecriture taille nombre lettres
+			foutF.write(0x00);
+			foutF.write(String.valueOf(numberOfLetterThatCanBeDiscover).getBytes()); // Ecriture taille nombre lettres
 																					// nécéssaire pour découvrir mot
-			fout.write(0x00);
-			fout.write(String.valueOf(displayFoundWords).getBytes()); // Ecriture taille displayFoundWord
-			fout.write(0x00);
-			fout.write(String.valueOf(displayFinalAnswer).getBytes()); // Ecriture taille displayAnswer
-			fout.write(0x00);
-			fout.write(String.valueOf(TimeLimitInMinute).getBytes()); // Ecriture taille TimeLimit
+			foutF.write(0x00);
+			foutF.write(String.valueOf(displayFoundWords).getBytes()); // Ecriture taille displayFoundWord
+			foutF.write(0x00);
+			foutF.write(String.valueOf(displayFinalAnswer).getBytes()); // Ecriture taille displayAnswer
+			foutF.write(0x00);
+			foutF.write(String.valueOf(TimeLimitInMinute).getBytes()); // Ecriture taille TimeLimit
 
-			while (fin.available() != 0) { // Ecriture de la vidéo depuis le fichier tampon
-				bytesRead = fin.read(buffer);
-				fout.write(buffer, 0, bytesRead);
+			while (finF.available() != 0) { // Ecriture de la vidéo depuis le fichier tampon
+				bytesRead = finF.read(bufferF);
+				foutF.write(bufferF, 0, bytesRead);
 			}
-			fout.write(exoName.getBytes()); // Ecriture du texte
-			fout.write(consigne.getBytes()); // Ecriture du texte
-			fout.write(script.getBytes()); // Ecriture du texte
-			fout.write(aide.toString().getBytes()); // Ecriture du texte
-			fout.write(occultationChar.getBytes()); // Ecriture du texte
-			fout.write(mode.getBytes()); // Ecriture du texte
-			fout.write(authorizeIncompleteBooleanWord); // Ecriture du texte
-			fout.write(numberOfLetterThatCanBeDiscover); // Ecriture du texte
-			fout.write(displayFoundWords); // Ecriture du texte
-			fout.write(displayFinalAnswer); // Ecriture du texte
-			fout.write(TimeLimitInMinute); // Ecriture du texte
+			foutF.write(exoName.getBytes()); // Ecriture du texte
+			foutF.write(consigne.getBytes()); // Ecriture du texte
+			foutF.write(script.getBytes()); // Ecriture du texte
+			foutF.write(aide.toString().getBytes()); // Ecriture du texte
+			foutF.write(occultationChar.getBytes()); // Ecriture du texte
+			foutF.write(mode.getBytes()); // Ecriture du texte
+			foutF.write(authorizeIncompleteBooleanWord); // Ecriture du texte
+			foutF.write(numberOfLetterThatCanBeDiscover); // Ecriture du texte
+			foutF.write(displayFoundWords); // Ecriture du texte
+			foutF.write(displayFinalAnswer); // Ecriture du texte
+			foutF.write(TimeLimitInMinute); // Ecriture du texte
 			
 //			System.out.println("Fichier écrit sans problème : ");
-		} catch (Exception e) {
-			System.out.println("Something went wrong! Reason: " + e.getMessage());
-		}
+
 
 	}
 
--- PTS2/src/application/SaveFileController.java
+++ PTS2/src/application/SaveFileController.java
@@ -1,24 +1,27 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class SaveFileController extends ParentController implements Initializable{
 
 	@FXML BorderPane bPane;
-	@FXML Label consigne_lbl, script_lbl, aide_lbl, media_lbl, occult_lbl, incomplet_lbl, solution_lbl, sauvegarde_lbl, mode_lbl;
-	private static String nomExo, consigne, script, aide, media, sauvegarde, mode;
-	private static int incomplet, tempsLimite;
-	private static char occult;
-	private static boolean solution, affichageMots;
+	@FXML Label nomExo_lbl, consigne_lbl, script_lbl, aide_lbl, media_lbl, occult_lbl, incomplet_lbl, affichageMots_lbl, solution_lbl, sauvegarde_lbl, mode_lbl, tempsLimite_lbl;
+	private String nomExo, consigne, script, aide, media, sauvegarde, mode;
+	private int incomplet, tempsLimite;
+	private char occult;
+	private boolean solution, affichageMots;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -27,6 +30,7 @@ public class SaveFileController extends ParentController implements Initializabl
 	
 	@Override
 	public void setMenuBar() {
+		// Pas de menu bar ici
 	}
 	
 	
@@ -71,21 +75,77 @@ public class SaveFileController extends ParentController implements Initializabl
 		this.tempsLimite = tempsLimite;
 		
 		
+		this.nomExo_lbl.setText(nomExo);
 		this.consigne_lbl.setText(consigne);
 		this.script_lbl.setText(script);
 		this.aide_lbl.setText(aide);
 		this.media_lbl.setText(media);
 		this.occult_lbl.setText(String.valueOf(occult));
-		this.incomplet_lbl.setText(String.valueOf(incomplet));
-		this.solution_lbl.setText(String.valueOf(solution));
 		this.sauvegarde_lbl.setText(sauvegarde);
 		this.mode_lbl.setText(mode);
+		this.affichageMots_lbl.setText(String.valueOf(affichageMots));
+		this.incomplet_lbl.setText(String.valueOf(incomplet));
+		this.solution_lbl.setText(String.valueOf(solution));
+		this.tempsLimite_lbl.setText(String.valueOf(tempsLimite)+"s");
+		
 	}
 	
 	@FXML 
 	public void SaveFile(){
 		List<String> aideFinal = Arrays.asList(aide.split(" +"));
-		new FileManager().sauvegarderFichier(nomExo, sauvegarde, consigne, script, aideFinal, media, String.valueOf(occult), mode, affichageMots ? 1 : 0, incomplet, 1 ,solution  ? 1 : 0, tempsLimite);
+		Stage stage = (Stage) bPane.getScene().getWindow();
+		if(nomExo != null						&& 
+				sauvegarde != null				&& 
+				consigne != null				&& 
+				script != null					&&
+				aideFinal != null				&&
+				media != null					&&
+				String.valueOf(occult) != null	&&
+				mode != null					&&
+				incomplet != 0) {
+			
+			try {
+				new FileManager().sauvegarderFichier(nomExo, sauvegarde, consigne, script, aideFinal, media, String.valueOf(occult), mode, affichageMots ? 1 : 0, incomplet, 1 ,solution  ? 1 : 0, tempsLimite);
+				
+				createValidationAlert(sauvegarde,nomExo);
+				stage.close();
+				Main.setScreen(0);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.err.println("Erreur inconnue");
+				
+				stage.close();
+				
+				Main.setScreen(0);
+			}
+		}
+		else {
+			System.err.println("Erreur dans la sauvegarde");
+			createErrorAlert();
+			stage.close();
+		}
+
+	}
+	
+	public void createErrorAlert() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION,
+				"Impossible de sauvegarder votre fichier, des champs sont manquants.\n"
+				+"Veuillez renseigner tous les champs.", ButtonType.OK);
+
+		alert.setHeaderText("La sauvegarde a échouée");
+		alert.setTitle("Erreur de sauvegarde");
+
+		alert.showAndWait();
+	}
+	public void createValidationAlert(String pathExo, String nomExo) {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION,
+				"Vous pouvez retrouver le fichier de l'exercice sur votre ordinateur à cet emplacement : \n"
+				+ pathExo+"/"+nomExo+".exo", ButtonType.OK);
+
+		alert.setHeaderText("Votre fichier a été sauvegardé avec succès");
+		alert.setTitle("Succès");
+
+		alert.showAndWait();
 	}
 
 
